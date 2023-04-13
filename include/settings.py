a302070951b58c2ebc6e66bb82a2068d0225b039
Dino Source <dinosource.cpp@gmail.com>
2023-04-13 12:06:19 +0400

    Modify screen size (from 1920x1080 to 1200x800)

--- include/settings.py
+++ include/settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self) -> None:
         """ Initialize the game's settings """
         # Screen settings
-        self.screen_width = 1920
-        self.screen_height = 1080
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
