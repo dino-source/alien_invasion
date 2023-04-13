34b4be9f1ef1226157335b198f78a5a62294bc56
Dino Source <dinosource.cpp@gmail.com>
2023-04-13 18:43:52 +0400

    Add moving right feature

--- include/ship.py
+++ include/ship.py
@@ -5,16 +5,24 @@ class Ship:
     """ Class to manage the ship """
     
     def __init__(self, ai_game) -> None:
-        """ Initialize the ship and set its starting position """
+        # Initialize the ship and set its starting position
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         
-        """ Load the ship image and get its rect """
+        # Load the ship image and get its rect
         self.image = pygame.image.load("resources/images/ship.bmp")
         self.rect = self.image.get_rect()
 
-        """ Start each new ship at the bottom center of the screen """
+        # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Movement flag; start with a ship that's not moving
+        self.moving_right = False
+
+    def update(self):
+        # Update the ship position based on the movement flag
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """ Draw the ship at its current location """
