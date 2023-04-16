dc784aa8da0cc0a09d454defbd4411b445264aed
Dino Source <dinosource.cpp@gmail.com>
2023-04-16 22:37:30 +0400

    Add 'Play' button

--- include/alien_invasion.py
+++ include/alien_invasion.py
@@ -2,8 +2,11 @@ import sys
 import pygame
 
 from time import sleep
+
+from pygame.draw import rect
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -31,7 +34,10 @@ class AlienInvasion:
         self._create_fleet()
 
         # Start Alien Invasion in active state
-        self.game_active = True
+        self.game_active = False 
+
+        # Make the Play button
+        self.play_button = Button(self, "Play")
 
 
     def run_game(self):
@@ -55,6 +61,26 @@ class AlienInvasion:
                 self._check_keydown_events(event.key, True)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event.key, False)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+
+    def _check_play_button(self, mouse_pos):
+        """ Start a new game when the player click Play """
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            # Reset the game statistics
+            self.stats.reset_stats()
+            self.game_active = True
+
+            # Get rid of any remaining bulets and aliens
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
 
     def _check_keyup_events(self, event_key, moving_flag):
@@ -196,6 +222,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive
+        if not self.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
