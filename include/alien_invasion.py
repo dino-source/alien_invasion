68ed7254770e8b233b040667cc6f71ac6e642be6
Dino Source <dinosource.cpp@gmail.com>
2023-04-17 10:57:19 +0400

    Hide mouse cursor when 'Play' button is clicked

--- include/alien_invasion.py
+++ include/alien_invasion.py
@@ -70,6 +70,9 @@ class AlienInvasion:
         """ Start a new game when the player click Play """
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Hide mouse cursor
+            pygame.mouse.set_visible(False)
+
             # Reset the game statistics
             self.stats.reset_stats()
             self.game_active = True
